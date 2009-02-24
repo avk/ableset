372f4f5bb0b9f163b1bc1a1fe7e3385c8f98b1a5
Arthur Klepchukov <arthur.klepchukov@gmail.com>
2009-02-24 01:59:36 -0800

    skills can't be null and are now unique for every user

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,6 +2,9 @@ class Skill < ActiveRecord::Base
 
   belongs_to :user
 
+  validates_presence_of :name
+  validates_uniqueness_of :name, :scope => [:user_id]
+
   def self.from_linked_in_profile(url)
     # validate as LinkedIn URL
     uri = URI.parse(url)
