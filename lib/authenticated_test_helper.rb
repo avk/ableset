d3320f1ef2b66481f59771c4a80ae22eb7b20209
Arthur Klepchukov <arthur.klepchukov@gmail.com>
2009-03-03 17:23:13 -0800

    full coverage on lib/authenticated_test_helper.rb

--- lib/authenticated_test_helper.rb
+++ lib/authenticated_test_helper.rb
@@ -4,8 +4,8 @@ module AuthenticatedTestHelper
     @request.session[:user_id] = user ? users(user).id : nil
   end
 
-  def authorize_as(user)
-    @request.env["HTTP_AUTHORIZATION"] = user ? ActionController::HttpAuthentication::Basic.encode_credentials(users(user).email, 'monkey') : nil
-  end
+  # def authorize_as(user)
+  #   @request.env["HTTP_AUTHORIZATION"] = user ? ActionController::HttpAuthentication::Basic.encode_credentials(users(user).email, 'monkey') : nil
+  # end
   
 end
